827ce19c8786c6ac2c74e193589e979d868f0528
Daniel Souza <danielrsouza10@gmail.com>
2023-06-05 09:00:31 -0300

    add some variables to store the images and test some events

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -3,21 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.css'],
 })
 export class MainComponent {
-  numberOfItems: number = 0;
 
-  onPlusButton(){
+  numberOfItems: number = 0;
+  mainImage: string = 'assets/images/image-product-1.jpg';
+  images: string[] = [
+    'assets/images/image-product-1-thumbnail.jpg',
+    'assets/images/image-product-2-thumbnail.jpg',
+    'assets/images/image-product-3-thumbnail.jpg',
+    'assets/images/image-product-4-thumbnail.jpg',
+  ];
+
+  onPlusButton() {
     this.numberOfItems++;
   }
 
-  onMinusButton(){
-    if(this.numberOfItems > 0){
-      this.numberOfItems--;
-    } else{
-      this.numberOfItems = 0;
-    }
-    
+  onMinusButton() {
+    this.numberOfItems > 0 ? this.numberOfItems-- : (this.numberOfItems = 0);
+  }
+
+  changeImage() {
+    alert('you click me');
   }
 }
